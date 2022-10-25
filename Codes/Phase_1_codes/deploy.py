b05ee2c9d04f83ccb106860a255d1551c890260e
shreyashvi <dell@DESKTOP-SP0IHN8.localdomain>
2022-10-25 18:30:28 +0530

    text file

---
+++ Codes/Phase_1_codes/deploy.py
@@ -0,0 +1,88 @@
+from PIL import Image
+import onnx
+import numpy as np
+from torchvision.models.quantization import mobilenet as qmobilenet
+import torch
+
+import tvm
+from tvm import relay
+from tvm.contrib.download import download_testdata
+
+
+def get_transform():
+    import torchvision.transforms as transforms
+
+    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    return transforms.Compose(
+        [
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            normalize,
+        ]
+    )
+def get_real_image(im_height, im_width):
+    img_path='fish.png'
+    return Image.open(img_path).resize((im_height, im_width))
+
+def get_imagenet_input():
+    im = get_real_image(224, 224)
+    preprocess = get_transform()
+    pt_tensor = preprocess(im)
+    return np.expand_dims(pt_tensor.numpy(), 0)
+
+def get_synset():
+    synset_url = "".join(
+        [
+            "https://gist.githubusercontent.com/zhreshold/",
+            "4d0b62f3d01426887599d4f7ede23ee5/raw/",
+            "596b27d23537e5a1b5751d2b0481ef172f58b539/",
+            "imagenet1000_clsid_to_human.txt",
+        ]
+    )
+    synset_name = "imagenet1000_clsid_to_human.txt"
+    synset_path = download_testdata(synset_url, synset_name, module="data")
+    with open(synset_path) as f:
+        return eval(f.read())
+
+
+
+
+
+synset = get_synset()
+inp = get_imagenet_input()
+
+def quantize_model(model, inp):
+    model.fuse_model()
+    model.qconfig = torch.quantization.get_default_qconfig("fbgemm")
+    torch.quantization.prepare(model, inplace=True)
+    # Dummy calibration
+    model(inp)
+    torch.quantization.convert(model, inplace=True)
+qmodel = qmobilenet.mobilenet_v2(pretrained=True).eval()
+pt_inp = torch.from_numpy(inp)
+quantize_model(qmodel, pt_inp)
+script_module = torch.jit.trace(qmodel, pt_inp).eval()
+
+with torch.no_grad():
+    pt_result = script_module(pt_inp).numpy()
+
+
+def run_tvm_model(mod, params, input_name, inp, target="llvm"):
+    with tvm.transform.PassContext(opt_level=3):
+        lib = relay.build(mod, target=target, params=params)
+    lib.export_library("result.tar")
+    lib.export_library("result.so")
+    runtime = tvm.contrib.graph_executor.GraphModule(lib["default"](tvm.device(target, 0)))
+    runtime.set_input(input_name, inp)
+    runtime.run()
+    return runtime.get_output(0).numpy(), runtime
+
+input_name = "input"  
+input_shapes = [(input_name, (1, 3, 224, 224))]
+mod, params = relay.frontend.from_pytorch(script_module, input_shapes)
+
+target = "llvm"
+tvm_result, tvm_mod = run_tvm_model(mod, params, input_name, inp, target=target)
+output_labels = np.argsort(tvm_result[0])[::-1][:5]
+print("Predicted top 5 lables:- ", [synset[label] for label in output_labels])
